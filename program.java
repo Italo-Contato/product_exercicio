9423588eb4c2c93abc7cb97befc7e9385c5ff81b
Gabriel Italo <gabriel.italo.contato@gmail.com>
2023-07-02 00:02:40 -0300

    Retirada variavel não utilizada, classes comentadas, linhas antes comentadas agora fazem parte do codigo util

--- program.java
+++ program.java
@@ -5,6 +5,8 @@ import entities.Product;
 public class program {
 
 	public static void main(String[] args) {
+		//classe que é responsavel pelo gerenciamento de produtos em estoque
+		
 		Scanner reading = new Scanner(System.in);
 		
 		System.out.println("Name: ");
@@ -13,20 +15,20 @@ public class program {
 		System.out.println("Preço: ");
 		double price = reading.nextDouble();
 		
-		Product Produto1 = new Product(nome, price);
+		Product produto1 = new Product(nome, price);
 		
-		System.out.println(Produto1);
+		System.out.println(produto1);
 		
 		
 		
 		System.out.println("Enter the number of products to be added in stock: ");
-		Produto1.addProduto(reading.nextInt());
+		produto1.addProduto(reading.nextInt());
 		
-		System.out.println(Produto1); 
-		/*System.out.println("Updated data: "+ p1);
+		System.out.println(produto1); 
+		System.out.println("Updated data: "+ produto1);
 		System.out.println("Enter the number of products to be removed from stock: ");
-		p1.retirarProduto(reading.nextInt());
-		System.out.println("Updated data: "+ p1);		*/
+		produto1.retirarProduto(reading.nextInt());
+		System.out.println("Updated data: "+ produto1);		
 		
 
 	}
